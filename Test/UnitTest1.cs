a348ce11043b9a081c7118865bb7db25cf021449
Andrew Samuel A <agabaiduandrew@gmail.com>
2025-03-07 09:13:31 +0000

    Add project files.

---
+++ Test/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+        IWebDriver driver =new ChromeDriver();
+        driver.Navigate().GoToUrl("https://demoqa.com/");
+        driver.Manage().Window.Maximize();
+        Thread.Sleep(5000);
+        driver.Quit();
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
